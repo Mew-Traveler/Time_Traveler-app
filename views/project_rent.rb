dca1961c2cd78c645a657d372febb6d3c385e417
NTHUMan <q2221252@gmail.com>
2016-11-26 22:07:24 +0800

    project_rent.rb for this time

---
+++ views/project_rent.rb
@@ -0,0 +1,14 @@
+doctype html
+html
+  head
+    title TimeTraveler
+
+    / Bootstrap CSS and theme (Cerulean Theme)
+    link{
+      href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/united/bootstrap.min.css"
+      rel="stylesheet" integrity="sha384-fUMURLTdEcpeYHly3PAwggI3l2UvdHNg/I+8LRph7hLDcAZm77YfDx3Tjum9d3vK"
+      crossorigin="anonymous"
+    }
+    / Custom CSS
+    link rel='stylesheet' href='/style.css'
+  body
