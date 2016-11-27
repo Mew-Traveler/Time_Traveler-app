c87d1186bb3d68908b48ba1f1fde86383f48281e
Yvonne <tailcloud14916@gmail.com>
2016-11-28 00:57:33 +0800

    return tmpView as rackup

--- controllers/project_rent.rb
+++ controllers/project_rent.rb
@@ -3,12 +3,12 @@
 # TimeTraveler web service
 class TimeTravelerApp < Sinatra::Base
   get "/?" do
-    result = TimeTraveler.call
-    if result.success?
-      @data = result.value
-    else
-      flash[:error] = result.value.message
-    end
+    # result = TimeTraveler.call
+    # if result.success?
+    #   @data = result.value
+    # else
+    #   flash[:error] = result.value.message
+    # end
+    slim :tmpView
   end
-  slim :project_rent
 end
