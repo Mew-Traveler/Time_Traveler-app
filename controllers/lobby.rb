6aba797d5cda343edba3a7284aab68ac7eac576a
NTHUMan <q2221252@gmail.com>
2016-12-23 15:30:05 +0800

    can get dailyplan data

--- controllers/lobby.rb
+++ controllers/lobby.rb
@@ -6,8 +6,17 @@ class TimeTravelerApp < Sinatra::Base
     @projectName = params[:projectName]
     @totalDays = params[:projectDays]
     @userEmail = params[:userEmail]
-    #dailyplan = GetDailyplan.call(params)
+    result = GetDailyplan.call(params)
     
+    if result.success?
+       dailyplan = result.value
+       @dailyplan_info = dailyplan["dailyplan_info"]
+       @house_info = dailyplan["house_info"]
+       puts @house_info
+    else
+
+    end
+
     slim :project_rent
   end
 end
--- services/get_dailyplan.rb
+++ services/get_dailyplan.rb
@@ -1,23 +1,26 @@
 # frozen_string_literal: true
 
-class CreateUser
+class GetDailyplan
   extend Dry::Monads::Either::Mixin
   extend Dry::Container::Mixin
 
   def self.call(get_dailyplan_request)
     Dry.Transaction(container: self) do
       step :get_dailyplan_from_api
-    end.call(create_request)
+    end.call(get_dailyplan_request)
   end
 
 
-  register :call_api_to_create_user, lambda { |get_dailyplan_request|
+  register :get_dailyplan_from_api, lambda { |get_dailyplan_request|
     userEmail = get_dailyplan_request[:userEmail]
     project_id = get_dailyplan_request[:project_id]
     nthday = get_dailyplan_request[:nthday]
     http_result = HTTP.get("#{TimeTravelerApp.config.Time_Traveler_API}/project/#{userEmail}/#{project_id}/#{nthday}")
     if http_result.status == 200
-      Right(http_result)
+      body = http_result.body
+      dailyplan = JSON.parse(body)
+
+      Right(dailyplan)
     else
       Left(Error.new('The mail must be used by others.'))
     end
