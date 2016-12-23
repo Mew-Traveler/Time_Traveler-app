85b55d486bdc541f981d03e31172753dacdb832b
NTHUMan <q2221252@gmail.com>
2016-12-24 07:55:20 +0800

    show different dailyplan

--- controllers/lobby.rb
+++ controllers/lobby.rb
@@ -6,15 +6,17 @@ class TimeTravelerApp < Sinatra::Base
     @projectName = params[:projectName]
     @totalDays = params[:projectDays]
     @userEmail = params[:userEmail]
+    @nthday = params[:nthday]
+    @project_id = params[:project_id]
     result = GetDailyplan.call(params)
     
     if result.success?
        dailyplan = result.value
        @dailyplan_info = dailyplan["dailyplan_info"]
-       @house_info = dailyplan["house_info"]
-       puts @house_info
+       # @house_info = dailyplan["house_info"]
+       puts @dailyplan_info
     else
-
+      puts "wrong params"
     end
 
     slim :project_rent
