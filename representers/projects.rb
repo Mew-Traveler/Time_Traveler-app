3776fbf61ce9cd31531be5dd32ff1a83c3b731cd
wtlin1228 <wtlin0711@gmail.com>
2016-12-23 23:18:05 -0800

    add require_relative

--- representers/projects.rb
+++ representers/projects.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require_relative 'rent'
+require_relative 'project'
 
 # Represents overall group information for JSON API output
 class ProjectsRepresenter < Roar::Decorator
