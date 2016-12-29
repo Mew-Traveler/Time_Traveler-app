b85c81cc3d38e3db577e70c4f8ba9db4e88c60ac
NTHUMan <q2221252@gmail.com>
2016-12-29 16:37:04 +0800

    fix bug for create new project

--- controllers/new_project.rb
+++ controllers/new_project.rb
@@ -1,15 +1,48 @@
 class TimeTravelerApp < Sinatra::Base
 
   post "/create/newProject/?" do
+    @projectName = params[:projectName]
+    @userEmail = params[:userEmail]
+    @nthday = "1"  
+
     create_request = CreateProjectRequest.call(params)
     new_result = CreateProject.call(create_request)
+
+    project = new_result.value
+    @totalDays = project.projects[0][:day]
+    @project_id = project.projects[0][:id]
+
+    params[:nthday] = @nthday
+    params[:project_id] = @project_id
+    params[:projectDays] = @totalDays
+
+    get_result = GetDailyplan.call(params)
     if new_result.success?
-      puts "the last"
-      project = new_result.value
-      puts project.projects[0][:day]
-      puts project.projects[0][:projectName]
-      @totalDays = project.projects[0][:day]
-      # flash[:notice] = 'Group successfully added'
+      dailyplan = get_result.value
+      @dailyplan_info = dailyplan["dailyplan_info"]
+      @house_info = dailyplan["house_info"]
+  
+      # @dailyplan_info = {
+      #   project_id: project.projects[0][:id],
+      #   roomId: "empty",
+      #   nthday: "1",
+      #   date: project.projects[0][:dateStart],
+      #   timeStart: "empty",
+      #   timeEnd: "empty",
+      #   locateStart: "empty",
+      #   locateEnd: "empty",
+      #   timeRemain: "empty",
+      # }
+      # @house_info = {
+      #   roomId: "empty",
+      #   roomName: "empty",
+      #   roomPrice: "empty",
+      #   address: "empty",
+      #   airbnb_link: "empty",
+      #   roomImg: "empty",
+      #   bed: "empty",
+      #   roomRank: "empty",
+      # }
       slim :project_rent
     else
       flash[:error] = result.value.message
